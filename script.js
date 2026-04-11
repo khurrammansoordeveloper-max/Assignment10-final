008fedf6a0c6ce684478211439eeac9c9de5d742
Khurram Mansoor <khurram.mansoor.developer@gmail.com>
2026-04-10 23:44:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function greet1() {
+  console.log("Hello");
+}
+greet1();
+greet1();
+greet1();
+
+
+function greet2() {
+  console.log("Welcome Students!");
+}
+greet2();
+
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+greet("Ali");
+greet("Ayesha");
